6cd7290fb68b51b8e50172c1a9f9c1116e78e2a6
Taylon Silva <taylon.92pellegue@gmail.com>
2026-05-15 15:26:01 -0400

    Obtendo os dados do formulário

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -23,4 +26,8 @@ function formatCurrencyBRL(value){
 
   // Retorna o valor formatado
   return value
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault() // Impede o envio do formulário
 }
